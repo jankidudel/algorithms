b2a771e998d8a7cb5a6da7ea2b0914c96e559f9e
jankidudel <jankidudel@gmail.com>
2023-09-03 21:17:47 +0300

    bubble sort slight modification

--- bubbleSort.ts
+++ bubbleSort.ts
@@ -10,30 +10,17 @@ function bubbleSort(input: number[], order: SortOrder): number[] {
     do {
        swapped = false;
        for (let i = 1; i < inputLength; i++) {
-            switch (order) {
-                case SortOrder.Asc:
-                    if (input[i-1] > input[i]) {    
-                        let tmpVal = input[i-1];
-                        input[i-1] = input[i];
-                        input[i] = tmpVal; 
-                        swapped = true;
-                    }
-                    break;
-                case SortOrder.Desc:
-                    if (input[i] > input[i-1]) {    
-                        let tmpVal = input[i];
-                        input[i] = input[i-1];
-                        input[i-1] = tmpVal; 
-                        swapped = true;
-                    }
-                    break;
+            let swapNeeded = order === SortOrder.Asc ? (input[i-1] > input[i]) :  (input[i-1] < input[i])
+            if (swapNeeded) {    
+                let tmpVal = input[i-1];
+                input[i-1] = input[i];
+                input[i] = tmpVal; 
+                swapped = true;
             }
        }
     } while (swapped);
     return input;
 }
-
-
 
 const inputArr = [3, 8, 9, 6, 10, 7, 1, 5, 4, 2];
 
